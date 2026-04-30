d8e86a710e4712c9b7a0b21be382346a0920e475
njleach <njleach.1@gmail.com>
2026-04-30 11:40:30 +0100

    test: add numba test

--- tests/test_scaling.py
+++ tests/test_scaling.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from precip_extremes_scaling import scaling
+from precip_extremes_scaling import scaling, scaling_nb
 
 
 def test_scaling_matches_readme_example() -> None:
@@ -13,3 +13,13 @@ def test_scaling_matches_readme_example() -> None:
     precip = scaling(omega, temp, plev, ps)
 
     assert precip == pytest.approx(6.1e-05, rel=1e-2)
+
+def test_scaling_numba() -> None:
+    omega = np.linspace(-0.3, 0.2, 10)
+    temp = np.linspace(270, 220, 10)
+    plev = np.linspace(100000, 10000, 10)
+    ps = 100000
+
+    precip = scaling_nb(omega, temp, plev, ps)
+
+    assert precip == pytest.approx(6.66e-05, rel=1e-2)
